ef94c93b8dae8071d75f7844d8613e464368d794
Anders Wind <awia00@gmail.com>
2016-02-26 21:39:51 +0100

    started working on the simplify

--- src/DCR/HistoryConsensus/Graph.fs
+++ src/DCR/HistoryConsensus/Graph.fs
@@ -35,4 +35,37 @@ module Graph =
                     Edges = List.ofSeq (Seq.where (fun actionId -> actionId <> toNode.Id) (Seq.ofList fromNode.Edges))
             }) graph.Nodes }
 
+    let getNode graph actionId : Action = Map.find actionId graph.Nodes
+    let getNodes graph actionIdList = List.map (getNode graph) actionIdList
+    let graphNodesToList graph = Map.toList (graph.Nodes)
+
     let empty : Graph = { Nodes = Map.empty }
+
+
+    let getBeginningNodes graph : Action list = 
+        let rec calcToNodes (nodes:(ActionId*Action) list) (accList:ActionId list) : ActionId list =
+            match nodes with
+            | [] -> accList
+            | (x,y)::xs -> calcToNodes xs (List.foldBack (fun element list -> element::list) y.Edges accList)
+        let toNodes = calcToNodes (graphNodesToList graph) []
+        let beginningNodesIds = List.except (List.map (fun (x,y) -> x) (graphNodesToList graph)) toNodes
+        getNodes graph beginningNodesIds
+
+    let transitiveClousure beginningNodes graph actionType = 
+        let rec transitiveClous (list:Action list) newGraph = 
+            match list with
+            | [] -> newGraph
+            | x::xs -> let rec inner edgeList innerGraph newXs =
+                        match edgeList with
+                        | [] -> transitiveClous newXs innerGraph
+                        | y::ys ->  let toNode = (getNode graph y)
+                                    if(toNode.Type = actionType)
+                                    then inner ys (addEdge (getNode graph x.Id) toNode innerGraph) (toNode::newXs)
+                                    else inner ys innerGraph newXs
+                       inner x.Edges newGraph xs
+        transitiveClous beginningNodes graph
+
+    let simplify (graph:Graph) (actionType:ActionType) : Graph =
+        let beginningNodes = getBeginningNodes graph
+        let graphWithTransClous = transitiveClousure beginningNodes graph actionType
+        empty 
