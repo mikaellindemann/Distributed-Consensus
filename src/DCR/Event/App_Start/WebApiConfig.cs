efc453300d353059a98d3159ad973ae2bb3d7fe1
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-03-07 15:31:37 +0100

    Try to fix Json issues.

--- src/DCR/Event/App_Start/WebApiConfig.cs
+++ src/DCR/Event/App_Start/WebApiConfig.cs
@@ -1,5 +1,10 @@
-﻿using System.Net.Http.Headers;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Event
 {
@@ -19,6 +24,46 @@ namespace Event
             );
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            TypeDescriptor.AddAttributes(
+                typeof(Tuple<string, int>), 
+                new TypeConverterAttribute(typeof(TupleConverter)));
+        }
+    }
+
+    public class TupleConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        // Overrides the ConvertFrom method of TypeConverter.
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var s = value as string;
+            if (s != null)
+            {
+                var v = s.Split(',');
+                return new Tuple<string, int>(v[0], int.Parse(v[1]));
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        // Overrides the ConvertTo method of TypeConverter.
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                var v = value as Tuple<string, int>;
+                if (v != null)
+                    return $"{v.Item1}, {v.Item2}";
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
         }
     }
 }
