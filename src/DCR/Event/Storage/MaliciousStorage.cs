662b7b3d6cf8c8a26b932f8783360462142f4afe
Anders Wind <awia00@gmail.com>
2016-04-13 13:54:06 +0200

    Basic setup for implementing malicious Calls

---
+++ src/DCR/Event/Storage/MaliciousStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Event.Interfaces;
+
+namespace Event.Storage
+{
+    public class MaliciousStorage : IMaliciousStorage
+    {
+        private readonly IEventContext _context;
+
+        /// <summary>
+        /// Constructor used for dependency injection (used for testing purposes)
+        /// </summary>
+        /// <param name="context">Context to be used by EventStorage</param>
+        public MaliciousStorage(IEventContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            _context = context;
+        }
+    }
+}
