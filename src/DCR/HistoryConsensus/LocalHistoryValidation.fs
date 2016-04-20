069ae8a8d815ab8b37c5d314adb3011c4fcffc55
Anders Fischer-Nielsen <andersfischern@me.com>
2016-04-20 14:06:42 +0200

    Implemented check for no incoming relations in history when executing.

--- src/DCR/HistoryConsensus/LocalHistoryValidation.fs
+++ src/DCR/HistoryConsensus/LocalHistoryValidation.fs
@@ -54,10 +54,52 @@ module LocalHistoryValidation =
         // Implementation specific first value. The timestamps of our database should never be less than either 0 or 1.
         checkOrder firstActionId -1
 
-    // Check that no ingoing actions can appear when executing.
+    // Check that no incoming actions appear when executing.
+    let checkIncomingRelationsWhenExecuting eventId (history:Graph) : Result<Graph, FailureT list> = 
+        
+        // Get execution chunks, as a list of [ExecutionStart Action, random Action, random Action, ExecutionFinish Action])
+        let executionChunks = 
+            // From a given ExecuteStart Action take following Actions in the history until an ExecutionFinish is found or the history is empty.
+            let rec takeNodesUntilExecuteFinish action acc = 
+                if (action.Type = ExecuteFinish || action.Edges.IsEmpty) then action::acc
+                else let newAcc = action::acc
+                     let act = (Graph.getNode history action.Edges.MinimumElement)
+                     takeNodesUntilExecuteFinish act newAcc
 
-    // Check that all relations appears inside execution
+            // When an ExecuteStart is encountered, generate a chunk of Actions until the next ExectuteFinish is encountered.
+            let generateChunkIfExecuteStart action acc : Map<Action, Action list> = 
+                if action.Type = ExecuteStart 
+                then Map.add action <| takeNodesUntilExecuteFinish action [] <| acc
+                else acc    
 
+            // Generate a list if execution chunks from the given history.
+            Map.fold (fun acc actionId _ -> generateChunkIfExecuteStart <| Graph.getNode history actionId <| acc) Map.empty history.Nodes
+                |> Map.toList
+                |> List.map snd
+        
+        // Check whether a given Action type is valid/allowed.
+        let hasValidType actionType = 
+            match actionType with 
+            | IncludedBy | ExcludedBy | SetPendingBy | CheckedConditon  -> false
+            | _                                                         -> true
+
+        // Check a list of execution chunks for validity.
+        let rec checkChunks (chunks:Action list list) = 
+            // Check Action types in a chunk for validity.
+            let checkChunk (chunk:Action list) = 
+                List.forall (fun a -> hasValidType a.Type) chunk
+            
+            // Check every chunk for validity. If an illegal chunk is discovered, add a Failure to the list of failures.
+            match chunks with 
+            | c::cs     -> let chunkIsValid = checkChunk c
+                           if chunkIsValid then checkChunks cs 
+                           else Failure [([eventId], HasWrongIngoingAction)]
+            | _         -> Success history 
+
+        checkChunks executionChunks
+
+
+    // Validating
     let giantLocalCheck input eventId allowedIngoingRelations allowedOutgoingRelations =
         input |> hasBeginningNodesValidation
             >>= checkLocalHistoryForLocalInformationAboutOthers eventId 
