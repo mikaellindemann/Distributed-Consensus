e55d057b29a250d05358f1285bc2294b86511fad
Anders Fischer-Nielsen <andersfischern@me.com>
2016-04-20 14:06:18 +0200

    Removed vaildation examples.

--- src/DCR/HistoryConsensus/HistoryValidation.fs
+++ src/DCR/HistoryConsensus/HistoryValidation.fs
@@ -25,31 +25,6 @@ module HistoryValidation =
         | Failure f -> Failure f
 
     let (>>=) resultInput func = bind func resultInput
-    
-    let validationExample = 
-        let failureInput = "2 A"
-        let successInput = "okaystring"
-
-        let noSpacesValidation input = 
-            if Seq.exists (fun c -> System.Char.IsWhiteSpace(c)) input
-            then Failure "Input contained spaces."
-            else Success input
-
-        let noNumberValidation input =
-            if Seq.exists (fun c -> System.Char.IsDigit(c)) input
-            then Failure "Input contains a number."
-            else Success input
-
-        let noUpperCaseValidation input =
-            if Seq.exists (fun c -> System.Char.IsUpper(c)) input
-            then Failure "Input contains an uppercase character."
-            else Success input
-
-        let failureResult = failureInput |> noSpacesValidation >>= noNumberValidation >>= noUpperCaseValidation
-        let successResult = successInput |> noSpacesValidation >>= noNumberValidation >>= noUpperCaseValidation
-        printf "Failure result: %O" failureResult
-        printf "Success result: %O" successResult
-        
 
     // If no beginning nodes exist, we have either an empty history, or a history with cycles
     let hasBeginningNodesValidation (history : Graph) : Result<Graph, FailureT list> = 
