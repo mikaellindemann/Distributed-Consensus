b183cd1e38df98fee385af00889d18d6efc632f7
Anders Wind <awia00@gmail.com>
2016-04-25 15:26:39 +0200

    Added another validation - perhaps with same problem of signature

--- src/DCR/HistoryConsensus/HistoryValidation.fs
+++ src/DCR/HistoryConsensus/HistoryValidation.fs
@@ -69,9 +69,23 @@ module HistoryValidation =
 
 
     let agreeOnAmtOfActions  (history1 : Graph)   (history2 : Graph)  : Result<(Graph*Graph), FailureT list> =
-        let eventId1 = (Map.pick (fun x y -> Some x) history1.Nodes)
-        let eventId2 = (Map.pick (fun x y -> Some x) history2.Nodes)
-        if ((Map.filter (fun actionId action -> action.CounterpartId = eventId2) history1.Nodes).Count = (Map.filter (fun actionId action -> action.CounterpartId = eventId1) history2.Nodes).Count)
+        let eventId1 = fst (Map.pick (fun x y -> Some x) history1.Nodes)
+        let eventId2 = fst (Map.pick (fun x y -> Some x) history2.Nodes)
+        if ((Map.filter (fun actionId action -> fst action.CounterpartId = eventId2) history1.Nodes).Count = (Map.filter (fun actionId action -> action.CounterpartId = fst eventId1) history2.Nodes).Count)
+        then
+            Success (history1, history2)
+        else 
+            Failure [([string eventId1; string eventId2], Malicious)];
+
+
+    let agreeOnActions  (history1 : Graph)   (history2 : Graph)  : Result<(Graph*Graph), FailureT list> =
+        let eventId1 = fst (Map.pick (fun x y -> Some x) history1.Nodes)
+        let eventId2 = fst (Map.pick (fun x y -> Some x) history2.Nodes)
+        let actionsAbout2From1 = Map.filter (fun actionId action -> fst action.CounterpartId = eventId2) history1.Nodes |> Map.toSeq
+        let actionsAbout1From2 = Map.filter (fun actionId action -> fst action.CounterpartId = eventId1) history2.Nodes |> Map.toSeq
+        
+        let zipped = Seq.zip actionsAbout2From1 actionsAbout1From2
+        if (Seq.exists (fun ((actionId1,action1), (actionId2,action2)) -> snd action2.Id = snd action1.CounterpartId && snd action1.Id = snd action2.CounterpartId ) zipped)
         then
             Success (history1, history2)
         else 
