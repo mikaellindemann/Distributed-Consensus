79ba049d07a0dbbbb69c764857119c0069b76ce3
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-04-26 16:30:43 +0200

    Initial version of DCR Simulator used to check against a DCR graph.

---
+++ src/DCR/HistoryConsensus/DCRSimulator.fs
@@ -0,0 +1,86 @@
+﻿namespace HistoryConsensus
+
+open Action
+open Graph
+open HistoryValidation
+
+module DCRSimulator =
+    type EventState = bool * bool * bool // Included * Pending * Executed
+    type DCRState = Map<EventId, EventState>
+    type DCRRules = Set<EventId * EventId * ActionType> // From * To * Type
+
+    let buildCounterMap history : Map<ActionId, int> =
+        Graph.fold 
+            (fun counterMap action ->
+                Set.fold 
+                    (fun counterMap' edge ->
+                        let newValue = (Map.find edge counterMap') + 1
+                        Map.add edge newValue counterMap')
+                    counterMap 
+                    action.Edges)
+            (Map.map (fun _ _ -> 0) history.Nodes) // This maps every actionId to 0
+            history
+
+    let updateStateForRelation state eventId relationType =
+        let (included, pending, executed) = Map.find eventId state
+
+        match relationType with
+        | SetsPending -> Map.add eventId (included, true, executed) state
+        | Includes    -> Map.add eventId (true, pending, executed)  state
+        | Excludes    -> Map.add eventId (false, pending, executed) state
+        | _ -> failwith "Wrong relation type in model!"
+
+    let isExecutable conditions state =
+        Set.fold 
+            (fun exec (_,conditionEventId,_) ->
+                let (included, _, executed) = Map.find conditionEventId state
+                exec && (not included || executed)) // If included and not executed -> Not executable.
+            true // Assume executable
+            conditions
+
+    let execute (state : DCRState) (rules : DCRRules) eventId : DCRState option =
+        let eventRules = Set.filter (fun (id,_,_) -> id = eventId) rules
+
+        let conditions = Set.filter (fun (_,_,relationType) -> relationType = ActionType.ChecksCondition) eventRules
+
+        if not <| isExecutable conditions state
+        then None // Not executable, this execution is illegal
+        else
+            // Executable -> Apply state update.
+            let (included, pending, _) = Map.find eventId state
+
+            Some <| Set.fold
+                (fun state' (_,toEventId,relationType) ->
+                    updateStateForRelation state' toEventId relationType)
+                (Map.add eventId (included, pending, true) state) // Set executed state of current event.
+                (Set.filter (fun (_,_,relationType) -> relationType <> ActionType.ChecksCondition) eventRules) // Only look at rules that are not conditions.
+
+    let updateCounterMap counterMap history actionId : Map<ActionId, int> =
+        let counterMap' = Map.remove actionId counterMap // Remove the action that was just executed.
+
+        Set.fold // Decrement all the actions that are pointed at by the executed action.
+            (fun map toActionId -> 
+                let newValue = (Map.find toActionId map) - 1
+                Map.add toActionId newValue map)
+            counterMap'
+            (Graph.getNode history actionId).Edges
+
+
+    let simulate (history : Graph) (initialState : DCRState) (rules : DCRRules) : Result<Graph, FailureT list> =
+        let initialCounterMap = buildCounterMap history
+        
+        let rec simulateExecution state counterMap : Result<Graph, FailureT list> =
+            if Map.isEmpty counterMap
+            then Success history
+            else
+                // Take the first action that has a counter of 0
+                let ((eventId, _) as actionId,_) = 
+                    Map.filter (fun _ count -> count = 0) counterMap |> Map.toSeq |> Seq.head
+
+                match execute state rules eventId with // Execute if possible
+                | None -> Failure [([eventId],Malicious)] // TODO: Change to illegal execution.
+                | Some state' -> 
+                    let counterMap' = updateCounterMap counterMap history actionId // Update counters after execution
+                    simulateExecution state' counterMap' // Recursive step
+
+        simulateExecution initialState initialCounterMap
--- src/DCR/HistoryConsensus/DCRSimulator.fsi
+++ src/DCR/HistoryConsensus/DCRSimulator.fsi
@@ -1,8 +1,12 @@
 ﻿namespace HistoryConsensus
 
 open Action
+open Graph
+open HistoryValidation
 
 module DCRSimulator =
-    type EventState = bool * bool * bool
+    type EventState = bool * bool * bool // Included * Pending * Executed
     type DCRState = Map<EventId, EventState>
+    type DCRRules = Set<EventId * EventId * ActionType> // From * To * Type
 
+    val simulate : Graph -> DCRState -> DCRRules -> Result<Graph, FailureT list> // History -> Initial State -> DCR Graph -> Result
